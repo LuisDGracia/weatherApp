bc143640bf6b6fae1092f754f502e18de8278b0e
Zuzki <ldegrac071@hotmail.com>
2020-10-04 20:11:42 -0500

    testing geolocation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [location, setLocation] = useState({
+    latitude: 0,
+    longitude: 0
+  })
+
+  useEffect(() => {
+    if( "geolocation" in navigator ){
+      navigator.geolocation.getCurrentPosition( (position) => {
+        setLocation({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+        })
+      })
+    }
+  }, [])
+
   return (
     <Fragment>
-      Hey!
+      {console.log(location)}
     </Fragment>
   );
 }
