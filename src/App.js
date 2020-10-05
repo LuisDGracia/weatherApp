7d067b6fd7e5c4dc89e063895e4a22a16629681b
Zuzki <ldegrac071@hotmail.com>
2020-10-05 15:57:35 -0500

    Changed data request to main

--- src/App.js
+++ src/App.js
@@ -1,32 +1,12 @@
-import React, { Fragment, useEffect, useState } from 'react';
+import Main from './Components/MainComponent/MainComponent';
+import React, { Fragment } from 'react';
 import './App.css';
-import axios from 'axios'
-import Main from './Components/MainComponent/mainComponent';
 
 function App() {
 
-  const [location, setLocation] = useState()
-
-  useEffect(() => {
-    // https://openweathermap.org/img/wn/{iconcode}@2x.png
-    if( "geolocation" in navigator ){
-      navigator.geolocation.getCurrentPosition( (position) => {
-        axios.get(`https://api.openweathermap.org/data/2.5/forecast?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric&cnt=7&appid=${process.env.REACT_APP_API_KEY}`)
-          .then((response) => {
-            setLocation(response.data);
-          })
-          .catch((err) => err);
-      })
-    }
-  }, [])
-
   return (
     <Fragment>
-      {location !== undefined ? 
-        <Main /> : 
-        <p>loading...</p>
-      }
-      
+      <Main />
     </Fragment>
   );
 }
--- src/Components/MainComponent/mainComponent.jsx
+++ src/Components/MainComponent/mainComponent.jsx
@@ -1,12 +1,35 @@
-import React from 'react'
-import { Container } from './mainStyles'
+import Today from '../TodayComponent/TodayComponent';
+import React, { useEffect, useState } from "react";
+import { Container } from './mainStyles';
+import axios from "axios";
 
 function Main() {
+
+  const [location, setLocation] = useState();
+
+  useEffect(() => {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        axios.get(`https://api.openweathermap.org/data/2.5/forecast?lat=${position.coords.latitude}&lon=${position.coords.longitude}&cnt=6&appid=${process.env.REACT_APP_API_KEY}`)
+        .then((response) => {
+          console.log(response.data)
+            setLocation(response.data);
+          })
+          .catch((err) => err);
+      });
+    }
+  }, []);
+
   return (
     <Container>
+      { location !== undefined ? 
+        <Today 
+          weatherTemp={ location.list[0].main } 
+          weatherInfo={ location.list[0].weather[0] } 
+          location={ location.city }/> : <p>loading...</p> }
       
     </Container>
-  )
+  );
 }
 
 export default Main
