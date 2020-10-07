50a7af548114c5d33720650eba8702f958c6b36e
Zuzki <ldegrac071@hotmail.com>
2020-10-07 18:46:48 -0500

    I'm not sure of this
    i think that doing this with api request is correct
    
    but at the moment i'm leaving this like it is

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
+import React, { Fragment, useEffect, useState } from 'react';
 import Main from './Components/MainComponent/MainComponent';
-import React, { Fragment } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
 
+  const [forecast, setForecast] = useState();
+  const [weather, setWeather] = useState();
+
+  useEffect(() => {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        axios.get(`https://api.openweathermap.org/data/2.5/forecast?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${process.env.REACT_APP_API_KEY}`)
+        .then((response) => {
+            setForecast(response.data);
+          })
+          .catch((err) => err);
+
+        axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${process.env.REACT_APP_API_KEY}`)
+        .then((response) => {
+            setWeather(response.data);
+          })
+          .catch((err) => err);
+      });
+    }
+  }, []);
+
+  console.log(weather)
+
   return (
     <Fragment>
-      <Main />
+      { forecast !== undefined ? 
+        <Main
+          weatherInfo={weather}
+          forecastInfo={forecast.list}/>
+      :  <p>loading...</p> }
     </Fragment>
   );
 }
