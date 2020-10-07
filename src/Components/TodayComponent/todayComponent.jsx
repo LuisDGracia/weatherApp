2beb303ea04a71c458ed4c2e27f84b667b9bc8b1
Zuzki <ldegrac071@hotmail.com>
2020-10-07 18:45:34 -0500

    Refactored
    done because of better undestanding of open weather api

--- src/Components/TodayComponent/todayComponent.jsx
+++ src/Components/TodayComponent/todayComponent.jsx
@@ -1,40 +1,41 @@
 import React, { useState } from 'react'
 import { Container, Icon } from './todayStyles'
 
-function Today({ weatherTemp, weatherInfo, location }) {
+function Today({ weatherInfo }) {
 
-  const [temp, setTemp] = useState('F');
+  const [temp, setTemp] = useState('C');
   let date = new Date();
   let day = [ 'Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat' ];
   let month = [ 'Jan', 'Feb', 'Mar', 'Apr' , 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec' ];
 
   const tempHandler = () =>{
-    let temperature = ""
 
     switch(temp){
       case 'C':
-        return temperature = `${Math.round((weatherTemp.temp - 273.15 ))}°${temp}`;
+        return `${Math.round((weatherInfo.main.temp - 273.15 ))}°${temp}`;
 
       case 'F':
-        return temperature = `${Math.round((weatherTemp.temp - 273.15) * (9/5) + 35)}°${temp}`;
+        return `${Math.round((weatherInfo.main.temp - 273.15) * (9/5) + 35)}°${temp}`;
 
       default:
-        return temperature = `${Math.round((weatherTemp.temp - 273.15 ))}°${temp}`;
+        return `${Math.round((weatherInfo.main.temp - 273.15 ))}°${temp}`;
     }
   }
 
   return (
     <Container>
-      <Icon src={`https://openweathermap.org/img/wn/${weatherInfo.icon}@4x.png`} alt={weatherInfo.description}></Icon>
-      <p>{ tempHandler() }</p>
-      <p> {weatherInfo.main} </p>
-      <p>{ day[ date.getDay() ] }</p>
-      <p>{ date.getDate() }</p>
-      <p>{ month[ date.getMonth() ] }</p>
-      <p>{ location.name }</p>
-      <p> {  location.country } </p>
+      <Icon
+        src={`https://openweathermap.org/img/wn/${weatherInfo.weather[0].icon}@4x.png`}
+        alt={weatherInfo.weather[0].description}/>
+        <p>{tempHandler()}</p>
+        <p> {weatherInfo.weather[0].main} </p>
+        <p>{day[date.getDay()]}</p>
+        <p>{date.getDate()}</p>
+        <p>{month[date.getMonth()]}</p>
+        <p>{weatherInfo.name}</p>
+        <p> {weatherInfo.sys.country} </p>
     </Container>
-  )
+  );
 }
 
 export default Today
