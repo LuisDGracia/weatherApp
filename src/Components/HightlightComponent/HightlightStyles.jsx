ce33c182ec9086da53481029e2831a9ddc60a5d9
Zuzki <ldegrac071@hotmail.com>
2020-10-09 16:37:00 -0500

    Highliht
    - Style updated for wind direction + icon
    
    - progress bar styled with bug

--- src/Components/HightlightComponent/HightlightStyles.jsx
+++ src/Components/HightlightComponent/HightlightStyles.jsx
@@ -5,31 +5,71 @@ export const HightlightContainer = styled.div`
   padding-top: 1.05rem;
   display: flex;
   flex-wrap: wrap;
-  justify-content: space-around;
+  justify-content: space-evenly;
   align-content: center;
 `
 
 export const HiglightContent = styled.div`
   width: 35%;
-  height: 40%;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
   background-color: #1e213a;
   margin-bottom: 2rem;
-  padding: 2rem;
+  padding: 1.1rem;
+  border-radius: 10px;
 
-  p{
+  p {
     margin: 0;
+    margin-bottom: 1.05rem;
   }
 
   .title {
     font-size: 1.5rem;
+    width: 100%;
     text-align: center;
     color: rgba(255, 255, 255, 0.7);
   }
 
   .data {
+    margin-top: 1.05rem;
+    width: 100%;
     font-size: 3rem;
     font-weight: bold;
     text-align: center;
     color: rgba(255, 255, 255, 0.7);
+  }
+
+`;
+
+export const Icon = styled.div`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+
+  font-size: 1.2rem;
+  text-align: center;
+  line-height: 40px;
+
+  color: rgba(255, 255, 255, 0.7);
+  background-color: rgba(255, 255, 255, 0.3);
+
+  ${({rotation}) => `transform: rotate(${rotation}deg);`}
+`;
+
+export const ProgressBar = styled.progress`
+  appearance: none;
+  width: 90%;
+  margin: 0 auto;
+  margin-top: 1.4rem;
+
+  ::-webkit-progress-value {
+    background: #2f80ed;
+    border-radius: 10px;
+  }
+
+  ::-webkit-progress-bar {
+    background: rgba(0, 0, 0, 0.05);
+    border-radius: 10px;
   }
 `;
--- src/Components/HightlightComponent/highlightComponent.jsx
+++ src/Components/HightlightComponent/highlightComponent.jsx
@@ -1,19 +1,31 @@
 import React from 'react'
-import { HightlightContainer, HiglightContent } from './HightlightStyles'
+import { HightlightContainer, HiglightContent, Icon, ProgressBar } from './HightlightStyles'
+import { faLocationArrow } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 function Highlight({ wind, pressure, humidity, visibility }) {
-  // ${wind.deg}deg
+  
+  const windDirection = () => {
+    let direction = ["N","NNE","NE","ENE","E","ESE", "SE", "SSE","S","SSW","SW","WSW","W","WNW","NW","NNW"];
+
+    return direction[Math.round(wind.deg/22.5)%16];
+  };
+
   return (
     <HightlightContainer>
       <HiglightContent>
         <p className="title">Wind status</p>
         <p className="data">{`${wind.speed} m/s`}</p>
+        <Icon rotation={wind.deg}>
+          <FontAwesomeIcon className="center" icon={faLocationArrow} />
+        </Icon>
+          <p>{windDirection()}</p>
       </HiglightContent>
 
       <HiglightContent>
         <p className="title">Humidity</p>
         <p className="data">{`${humidity}%`}</p>
-        <progress value={humidity} max={100} />
+        <ProgressBar value={humidity} max={100} />
       </HiglightContent>
 
       <HiglightContent>
