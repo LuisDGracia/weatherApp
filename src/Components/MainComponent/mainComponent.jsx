8bf9022f350c65a08aad3c8094b058b9c8a2e121
Zuzki <ldegrac071@hotmail.com>
2020-10-10 12:48:33 -0500

    Main
    Temperature is now handled here and style finished

--- src/Components/MainComponent/mainComponent.jsx
+++ src/Components/MainComponent/mainComponent.jsx
@@ -1,27 +1,47 @@
-import Today from '../TodayComponent/TodayComponent';
-import { ContentContainer, Content, MainContainer } from './mainStyles';
-import React from "react";
-import Forecast from '../ForecastComponent/ForecastComponent';
+import { ContentContainer, Content, MainContainer, TempContainer, Switch, Input, Slider } from './mainStyles';
 import Highlight from '../HightlightComponent/HighlightComponent';
+import Forecast from '../ForecastComponent/ForecastComponent';
+import Today from '../TodayComponent/TodayComponent';
+import React, {useState} from "react";
 
 function Main({ forecastInfo, weatherInfo }) {
 
+  const [temp, setTemp] = useState(true);
+
+  const tempHandler = () => {
+    
+    let nextTemp = !temp
+
+    setTemp( nextTemp );
+
+  }
+
   return (
     <MainContainer>
-      <Today weatherInfo={ weatherInfo }/>
+      <Today weatherInfo={weatherInfo} temp={temp} />
 
       <ContentContainer>
         <Content>
-          <Forecast forecast={ forecastInfo } />
-          <h1>Today's Higlights</h1>
-          <Highlight 
-            wind={ weatherInfo.wind }
-            humidity={ weatherInfo.main.humidity }
-            pressure={ weatherInfo.main.pressure }
-            visibility={ weatherInfo.visibility } />
-        </Content>
-      </ContentContainer>
 
+          <TempContainer>
+            <Switch>
+              <Input type="checkbox" className="checkbox" onClick={ tempHandler } />
+              <Slider className="slider" />
+            </Switch>
+          </TempContainer>
+
+          <Forecast forecast={forecastInfo} temp={temp} />
+          <h2 style={{ "color": '#ccc', "marginBottom": '10px' }} >Today's Higlights</h2>
+          <Highlight
+            wind={weatherInfo.wind}
+            humidity={weatherInfo.main.humidity}
+            pressure={weatherInfo.main.pressure}
+            visibility={weatherInfo.visibility}
+          />
+          <p className="copy" > &copy; Zuzki Made This </p>
+        </Content>
+
+      </ContentContainer>
     </MainContainer>
   );
 }
--- src/Components/MainComponent/mainStyles.jsx
+++ src/Components/MainComponent/mainStyles.jsx
@@ -1,13 +1,30 @@
 import styled from 'styled-components'
+import { device } from "../../mediaQueries";
 
 export const MainContainer = styled.div`
   width: 100%;
   display: flex;
+  flex-wrap: wrap;
   background-color: #100e1d;
+
+  @media ${device.laptop} {
+    flex-wrap: nowrap;
+  }
 `;
 
 export const ContentContainer = styled.section`
-  width: 70%;
+  width: 100%;
+
+  .copy {
+    margin: 0;
+    color: #ccc;
+    text-align: center;
+  }
+
+  @media ${device.laptop} {
+    width: 70%;
+    padding: 1.1rem;
+  }
 `;
 
 export const Content = styled.div`
@@ -16,4 +33,86 @@ export const Content = styled.div`
   flex-flow: column wrap;
   align-content: center;
   justify-content: space-around;
+`;
+
+// |||||||||||||||||||||||||||||||||||
+//            SLIDER
+// |||||||||||||||||||||||||||||||||||
+
+
+export const TempContainer = styled.div`
+  position: relative;
+  visibility: hidden;
+
+  @media ${device.laptop}{
+    visibility: visible;
+  }
+`;
+
+export const Switch = styled.label`
+  position: relative;
+  display: inline-block;
+  width: 80px;
+  height: 34px;
+  left: 90%;
+`;
+
+export const Input = styled.input`
+  opacity: 0;
+  width: 0;
+  height: 0;
+  transition: all 1s ease-in-out;
+
+  &:checked + .slider:before {
+    background-color: rgba(255, 255, 255, 0.5);
+    color: white;
+  }
+
+  &:checked + .slider:after {
+    background-color: white;
+    color: black;
+  }
+`;
+
+export const Slider = styled.span`
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  cursor: auto;
+  text-align: center;
+  line-height: 30px;
+  font-size: 1rem;
+  font-weight: bold;
+  transition: 0.4s;
+  border-radius: 20px;
+
+  :before {
+    position: absolute;
+    border-radius: 50%;
+    cursor: pointer;
+    content: "°C";
+    height: 30px;
+    width: 30px;
+    left: 4px;
+    bottom: 4px;
+    background-color: white;
+    transition: 0.4s;
+  }
+
+  :after {
+    position: absolute;
+    border-radius: 50%;
+    cursor: pointer;
+    content: "°F";
+    color: white;
+    height: 30px;
+    width: 30px;
+    right: 4px;
+    bottom: 4px;
+    background-color: rgba(255, 255, 255, 0.5);
+    transition: 0.4s;
+  }
 `;
